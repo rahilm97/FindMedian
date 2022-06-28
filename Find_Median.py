db65fe045a601ce5f237bd522d90bf9268c81f21
Rahil Mehta <rahilm97@hotmail.com>
2022-06-27 17:21:13 -0700

    Added additional test case

--- Find_Median.py
+++ Find_Median.py
@@ -3,6 +3,7 @@ arr2 = [1, 2, 3, 4]
 arr3 = [2]
 arr4 = [1, 9, 11, 18]
 arr5 = [9, 2, 17]
+arr6 = [1, 2, 399, 444, 5, 99999, 555, 8000]
 def findMedian(arr):
     arr.sort()
     n = len(arr)
@@ -10,9 +11,10 @@ def findMedian(arr):
         return ((arr[n // 2] + arr[n // 2 - 1]) / 2)
     else:
         return arr[n // 2]
-        
+
 print(findMedian(arr1))
 print(findMedian(arr2))
 print(findMedian(arr3))
 print(findMedian(arr4))
 print(findMedian(arr5))
+print(findMedian(arr6))
