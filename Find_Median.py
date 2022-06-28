2d86adc47cb39be8e8be8bd8b47fd65b77a6e80a
Rahil Mehta <rahilm97@hotmail.com>
2022-06-27 17:17:01 -0700

    Added the sort call

--- Find_Median.py
+++ Find_Median.py
@@ -1,12 +1,18 @@
 arr1 = [1, 2, 3, 4, 5]
 arr2 = [1, 2, 3, 4]
 arr3 = [2]
+arr4 = [1, 9, 11, 18]
+arr5 = [9, 2, 17]
 def findMedian(arr):
+    arr.sort()
     n = len(arr)
     if n % 2 == 0:
         return ((arr[n // 2] + arr[n // 2 - 1]) / 2)
     else:
         return arr[n // 2]
+        
 print(findMedian(arr1))
 print(findMedian(arr2))
 print(findMedian(arr3))
+print(findMedian(arr4))
+print(findMedian(arr5))
